962f0b5e40687d235f0d9ecb230107f5f8ba8d92
PiyushGaurav <piyushgaurav07@gmail.com>
2023-10-12 22:04:58 +0530

    added mention user logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 import './App.css';
-import users from './../data.js';
+import users from '../data';
 
 interface User {
-	id: string;
+	id: number;
 	first_name: string;
 	last_name: string;
 	email: string;
@@ -12,23 +12,64 @@ interface User {
 
 const App: React.FC = () => {
 	const [text, setText] = useState('');
-	const [mentionableUsers, setMentionableUsers] = useState<User[]>(users);
+	const [showMentions, setShowMentions] = useState(false);
+	const [mentionSearch, setMentionSearch] = useState('');
+	const [mentionableUsers, setMentionableUsers] = useState<User[]>([]);
+
+	const searchMentionUsers = () => {
+		const mentionedUsers = users.filter(user => {
+			return `${user.first_name.toLowerCase()} ${user.last_name.toLowerCase()}`.includes(mentionSearch.toLowerCase());
+		});
+		setMentionableUsers(mentionedUsers);
+	};
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			searchMentionUsers();
+		}, 200);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [mentionSearch]);
 
 	const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const newText = e.target.value;
 		setText(newText);
+
+		if (newText.includes('@')) {
+			const lastWord = newText.split(' ').pop() || '';
+			if (lastWord.startsWith('@')) {
+				console.log(lastWord.substring(1));
+				setMentionSearch(lastWord.substring(1));
+				setShowMentions(true);
+			} else {
+				setShowMentions(false);
+			}
+		} else {
+			setShowMentions(false);
+		}
 	};
 
-	mentionableUsers;
+	const handleMentionClick = (mention: User) => {
+		const newText = text.replace(new RegExp(`@${mentionSearch}` + '$'), `@${mention.first_name} ${mention.last_name} `);
+		setText(newText);
+		setShowMentions(false);
+	};
 
 	return (
 		<div className="content">
 			<input type="text" value={text} onChange={handleTextChange} placeholder="Write a comment..." />
-			<div className="mention-dropdown">
-				{mentionableUsers.map(user => (
-					<div key={user} className="users">{`${user.first_name} ${user.last_name}`}</div>
-				))}
-			</div>
+			{showMentions && (
+				<div className="mention-dropdown">
+					{mentionableUsers.map(user => (
+						<div
+							key={user.id}
+							className="users"
+							onClick={() => handleMentionClick(user)}
+						>{`${user.first_name} ${user.last_name}`}</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
