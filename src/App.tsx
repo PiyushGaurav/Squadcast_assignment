bff79c79d38b4d8ac14bb993eae996c8d6e649ac
PiyushGaurav <piyushgaurav07@gmail.com>
2023-10-12 21:00:15 +0530

    working on UI part

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState, ChangeEvent } from 'react';
 import './App.css';
+import users from './../data.js';
 
-function App() {
-	const [count, setCount] = useState(0);
+interface User {
+	id: string;
+	first_name: string;
+	last_name: string;
+	email: string;
+	gender: string;
+}
+
+const App: React.FC = () => {
+	const [text, setText] = useState('');
+	const [mentionableUsers, setMentionableUsers] = useState<User[]>(users);
+
+	const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const newText = e.target.value;
+		setText(newText);
+	};
+
+	mentionableUsers;
 
 	return (
-		<>
-			<h1>Hello</h1>
-		</>
+		<div className="content">
+			<input type="text" value={text} onChange={handleTextChange} placeholder="Write a comment..." />
+			<div className="mention-dropdown">
+				{mentionableUsers.map(user => (
+					<div key={user} className="users">{`${user.first_name} ${user.last_name}`}</div>
+				))}
+			</div>
+		</div>
 	);
-}
+};
 
 export default App;
